05c5337addbb93016d5d622129cf001ae52b32f1
ysytnik <yevhen.sytnik@gmail.com>
2019-02-06 14:27:37 +0100

    db.js modifying db link connect

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const MongoClient = require("mongodb").MongoClient;
 const ObjectID = require("mongodb").ObjectID;
 const dbname = "crud_mongodb";
 //const url = "mongodb://mongo:27017";
-const url = "mongodb://localhost:27017"
+const url = "mongodb://0.0.0.0:27017"
 const mongoOptions = { useNewUrlParser: true };
 
 const state = {
